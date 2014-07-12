3b84f8a071384e5a73b7df3522a3079a28714551
Panasan Sinroungrong <zerodare@gmail.com>
2014-07-13 01:41:00 +0700

    Create XMLParameter.php (Completed)

--- XMLParameter.php
+++ XMLParameter.php
@@ -13,9 +13,9 @@
  */
 class XMLParameter {
 
-    private $keyList = nil;
-    private $valueList = nil;
-    private $pointer;
+    private static $keyList;
+    private static $valueList;
+    private static $pointer;
 
     function __construct() {
         self::$keyList = array();
@@ -30,10 +30,50 @@ class XMLParameter {
     }
 
     public function pop(){
-        $key = pop(self::$keyList);
-        $val = pop(self::$valueList);
+        $key = self::$keyList[self::$pointer];
+        $val = self::$valueList[self::$pointer];
+        $array = array($key, $val);
+
+        self::$pointer++;
+        return $array;
+    } 
+
+    public function peek(){
+        $key = self::$keyList[self::$pointer];
+        $val = self::$valueList[self::$pointer];
         $array = array($key, $val);
 
         return $array;
-    } 
+    }
+
+    public function hasNext(){
+        return self::$pointer < sizeof(self::$keyList);
+    }
+
+    public function first(){
+        self::$pointer = 0;
+    }
+
+    public function last(){
+        self::$pointer = sizeof(self::$keyList);
+    }
+
+    public function getValue($key){
+        for($i = 0; i < sizeof(self::$keyList); $i++){
+            if(self::$keyList[i] === $key){
+                return self::$valueList[i];
+            }
+        }
+        return NULL;
+    }
+
+    public function clear(){
+        self::$keyList = NULL;
+        self::$valueList = NULL;
+
+        self::$keyList = array();
+        self::$valueList = array();
+        
+        self::first();
+    }
 }
--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <html>
     <body>
         <?php
+            include 'XMLParameter.php';
+
             $param = new XMLParameter();
             $param->add("aha", "vava1");
             $param->add("ahe", "vava2");
             $param->add("ahu", "vava3");
-            
-            for($i = 0; $i < 3; $i++){
-                echo $param->pop();
+            $param->add("aho", "vava4");
+
+            while($param->hasNext()){
+                $text = $param->pop();
+                echo '<p>key='.$text[0].' value='.$text[1].'</p>';
             }
+
+            $param->clear();
+            
+            echo '<h1>'.$param->hasNext().'</h1>';
         ?>
     </body>
 </html>
